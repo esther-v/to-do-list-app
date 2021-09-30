e521b0bd6a205fdc566f2413f9c7acd5bce6f108
esther-v <esvalas@gmail.com>
2021-09-30 15:18:06 +0200

    add filters

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('change', filterTodo);
 
 
 //functions
@@ -53,4 +55,29 @@ function deleteCheck(e){
     if(item.classList[0] === 'complete-btn'){
         todo.classList.toggle('completed');
     }
+}
+
+function filterTodo(e){
+    const todos = document.querySelectorAll('.todo')
+    todos.forEach(function(todo){
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                } else{
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                } else{
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    })
 }
